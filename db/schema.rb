32bb5c175a6a8e6add9117a2f73a0f80dda020f0
Rosalia <valenzuela.rosalia14@gmail.com>
2015-05-09 14:58:23 -0700

    adding comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509175005) do
+ActiveRecord::Schema.define(version: 20150509214313) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "eklabus", force: :cascade do |t|
     t.string   "name"
